94c9a10d0937c6d0aecbde1a4997f97a1b0427a2
tech-andgar <tech-andgar@users.noreply.github.com>
2025-10-08 04:10:22 +0000

    deploy: tech-andgar/image-to-pdf-client@5e642daf7b89d463729f79e6233f326e227b64b0

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/image-to-pdf-client/sw.js', { scope: '/image-to-pdf-client/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/image-to-pdf-client-public/sw.js', { scope: '/image-to-pdf-client-public/' })})}
